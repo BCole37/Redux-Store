ab39e08281a5a30783d129312b2a784e167aa5ec
Bry Cole <Brysonc98@live.com>
2022-01-22 10:04:18 -0800

    forgot to import global context

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,11 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import { Provider } from 'react-redux';
-import store from './utils/store';
+//Refactored Code
+//import { Provider } from 'react-redux';
+//import store from './utils/store';
 
+import { StoreProvider } from "./utils/StoreContext";
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
